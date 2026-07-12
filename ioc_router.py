2a61b291f9bcdacf81a56b6f38a9dc4f1435833e
AasiyaChauhan <aasiyachauhan23@gmail.com>
2026-07-11 22:48:55 -0400

    Implemented IOC routing for multiple indicator types

--- ioc_router.py
+++ ioc_router.py
@@ -1,18 +1,34 @@
 from ioc_parser import identify_ioc_type
-from threat_intel import check_ip, check_url
-
+from threat_intel import (
+    check_ip,
+    check_url,
+    check_domain,
+    check_hash
+)
 
 def scan_ioc(ioc):
 
     ioc_type = identify_ioc_type(ioc)
 
     if ioc_type == "IP Address":
+
         return check_ip(ioc)
 
     elif ioc_type == "URL":
+
         return check_url(ioc)
 
+    elif ioc_type == "Domain":
+
+        return check_domain(ioc)
+
+    elif ioc_type == "File Hash":
+
+        return check_hash(ioc)
+
     else:
+
         return {
-            "error": "IOC type not yet supported"
+            "error": "Unsupported IOC"
         }
+    
